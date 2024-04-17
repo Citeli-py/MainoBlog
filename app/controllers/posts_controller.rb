2f3ab5314d721d547520c90920707abd24db39f2
Citeli-py <matheus.citeli2001@gmail.com>
2024-04-17 15:29:09 -0300

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post= Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Object successfully created"
+      redirect_to @post
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:success] = 'Object was successfully deleted.'
+      redirect_to posts_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to posts_url
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+      if @post.update_attributes(params[:post])
+        flash[:success] = "Object was successfully updated"
+        redirect_to @post
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :author)
+  end
+
+end
