13e30488f8f6860d6bb07a083a74ef9eefc11d54
Citeli-py <matheus.citeli2001@gmail.com>
2024-04-18 14:07:23 -0300

    Fix comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_140437) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_170057) do
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "body"
