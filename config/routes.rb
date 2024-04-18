69da9829159ee90df6513b7dfd4fe9190dd77a2b
Citeli-py <matheus.citeli2001@gmail.com>
2024-04-18 15:30:53 -0300

    Criação de comentários anonimos ou não

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "posts#index"
-  resources :posts#, only: [:new, :show, :create]
+  resources :posts do
+    resources :comments
+  end
 end
