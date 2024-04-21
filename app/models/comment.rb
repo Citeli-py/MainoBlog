5ebde65ac302870886ac290b62a86dd41796788b
Citeli-py <matheus.citeli2001@gmail.com>
2024-04-20 22:01:47 -0300

    User_id em comentários e algumas correçoes nos comentários

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,19 @@
 class Comment < ApplicationRecord
   belongs_to :post
 
+
   validates :body, presence: true
+
+  def getAuthor
+    if user_id.nil?
+        return nil
+    end
+
+    User.find(user_id).name
+  end
+
+  def author?(current_user)
+    user_id == current_user.id
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_20_202428) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_205821) do
   create_table "comments", force: :cascade do |t|
-    t.string "author"
     t.text "body"
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -43,5 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_20_202428) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
